56dbac8b329f8a420542af25ce068c1c22fd5dfe
Mikhail <fedykov0@yandex.ru>
2021-03-27 19:16:27 +0300

    added tasks 2.1 and 2.3

---
+++ 2pract/prac2.1.py
@@ -0,0 +1,25 @@
+def f21(x = [0]*4):
+    if x[3] == 1989:
+        if x[1] == 'kit':
+            if x[2] == 1961:
+                return(0)
+            elif x[2] == 1967:
+                return(1)
+            elif x[2] == 1971:
+                return(2)
+        elif x[1] == 'fancy':
+            if x[2] == 1961:
+                return(3)
+            if x[2] == 1967:
+                return(4)
+            if x[2] == 1971:
+                return(5)
+        elif x[1] == 'ooc':
+            return(6)
+    elif x[3] == 2000:
+        if x[2] == 1961:
+            return(7)
+        if x[2] == 1967:
+            return(8)
+        if x[2] == 1971:
+            return(9)
