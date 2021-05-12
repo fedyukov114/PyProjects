16728d8f4cc36b9f0537e5d3092193733ad3c633
fedyukov114 <70742280+fedyukov114@users.noreply.github.com>
2021-05-12 19:09:07 +0300

    Delete 2pract3ex.py

--- 2pract3ex.py
+++
@@ -1,6 +0,0 @@
-myTable = "+7 423 584-6875 19.02.01 None 1.0 None Десиди А.Е. Десиди А.Е.\n+7 943 995-5607 25.10.04 None 0.1 None Зошский В.Р. Зошский В.Р.\nNone None None None None None None\n+7 984 406-8821 22.12.00 None 0.3 Сигли В.Г. Сигли В.Г.\n+7 640 416-1126 07.02.04 None 0.2 None Ризосберг Р.Ц. Ризосберг Р.Ц."
-print(myTable)
-print("ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ABOBA ")
-myTable = myTable.replace("None","")
-myTable = myTable.replace("  "," ")
-print(myTable)
